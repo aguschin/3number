7f04c7c5b8e8116959934b48973bc720ae7fb92b
aguschin <1aguschin@gmail.com>
2014-07-06 23:11:36 +0400

    rounded tiles, text "next", "steps"

--- js/game.js
+++ js/game.js
@@ -207,10 +207,50 @@ var check = function() {
 	return 1;
 }
 
-var drawRect = function(i,j,color) {
-	ctx.fillStyle = color;
-	ctx.fillRect(tile.x+i*tile.size, tile.y+j*tile.size, tile.size, tile.size);
-	ctx.strokeRect(tile.x+i*tile.size, tile.y+j*tile.size, tile.size, tile.size);	
+/**
+ * Draws a rounded rectangle using the current state of the canvas.
+ * If you omit the last three params, it will draw a rectangle
+ * outline with a 5 pixel border radius
+ * @param {CanvasRenderingContext2D} ctx
+ * @param {Number} x The top left x coordinate
+ * @param {Number} y The top left y coordinate
+ * @param {Number} width The width of the rectangle
+ * @param {Number} height The height of the rectangle
+ * @param {Number} radius The corner radius. Defaults to 5;
+ * @param {Boolean} fill Whether to fill the rectangle. Defaults to false.
+ * @param {Boolean} stroke Whether to stroke the rectangle. Defaults to true.
+ */
+function roundRect(ctx, x, y, width, height, radius, fill, stroke) {
+    if (typeof stroke == "undefined") {
+        stroke = true;
+    }
+    if (typeof radius === "undefined") {
+        radius = 5;
+    }
+    ctx.beginPath();
+    ctx.moveTo(x + radius, y);
+    ctx.lineTo(x + width - radius, y);
+    ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
+    ctx.lineTo(x + width, y + height - radius);
+    ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
+    ctx.lineTo(x + radius, y + height);
+    ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
+    ctx.lineTo(x, y + radius);
+    ctx.quadraticCurveTo(x, y, x + radius, y);
+    ctx.closePath();
+    if (stroke) {
+        ctx.stroke();
+    }
+    if (fill) {
+        ctx.fill();
+    }
+}
+
+var drawRect = function (i, j, color) {
+    ctx.fillStyle = color;
+    roundRect(ctx, tile.x + i * tile.size, tile.y + j * tile.size, tile.size * 0.9, tile.size * 0.9, 12, true, true);
+    //ctx.fillRect(tile.x+i*tile.size, tile.y+j*tile.size, tile.size, tile.size);
+    //ctx.strokeRect(tile.x+i*tile.size, tile.y+j*tile.size, tile.size, tile.size);
 }
 
 var drawText = function(i,j,n,scale) {
@@ -253,12 +293,12 @@ var render = function () {
 			drawText(i,j,tile.array[i][j],1);
 		}
 	}
-	chain.array = new Array();	
-	ctx.fillText(clicks, tile.x+32, 32);
-	ctx.fillText(tile.print(next), tile.x+288, 32);
+	chain.array = new Array();	    
+  ctx.fillText(clicks, tile.x + tile.size/2, 32);
+  ctx.fillText(tile.print(next), tile.x + tile.size*9/2, 32);
 	ctx.font = "14px Helvetica";
-	ctx.fillText('steps', tile.x+32, 52);
-	ctx.fillText('next', tile.x+288, 52);
+  ctx.fillText('steps', tile.x + tile.size/2, 52);
+  ctx.fillText('next', tile.x + tile.size*9/2, 52);
 	if (check()) {
 		ctx.fillText('press R for restart', tile.x+canvas.width/2, 32);
 	}
